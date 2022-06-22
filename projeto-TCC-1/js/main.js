2e2580d6e6b7f2ed808250ae9e59b0d95b1c3c5c
André Godoi Sorrens <61986097+andregodoisorrens@users.noreply.github.com>
2022-06-21 23:33:25 -0300

    coloquei o projeto no GitHub
    coloquei o projeto da pagina inicial no github

---
+++ projeto-TCC-1/js/main.js
@@ -0,0 +1,12 @@
+/*---------*/
+/* Função para abrir o menu */
+/*-------- */
+function abrir_menu() {
+    document.getElementById("id_menu").style.width = "50vw";
+}
+function fechar() {
+    document.getElementById("id_menu").style.width = "0";
+}
+function color(){
+    document.getElementById("id_menu").style.width="0";
+}
